2f02a2c04d3dab7ddf29a0536fdebd4763842046
fededotcom <fede.alvarez03@hotmail.com>
2023-10-24 11:31:38 -0300

    2

--- Proyecto-JavaTP/src/Stand.java
+++ Proyecto-JavaTP/src/Stand.java
@@ -1,4 +1,9 @@
-public class Stand {
+import java.util.ArrayList;
+public abstract class Stand {
 
-    
+    private String IDStand;
+    private int superficie;
+    private float precioM2;
+    private long IDCliente;
+    private ArrayList
 }
