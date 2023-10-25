2e072cfdd532f9bd3f9be65c331e6b24da70c5c5
Bruno <brunomenna2011@gmail.com>
2023-10-25 10:39:35 -0300

    creacion de Json y primera interfaz

---
+++ Proyecto-JavaTP/src/Principal/App.java
@@ -0,0 +1,9 @@
+package Principal;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Principal principal = new Principal();
+        principal.pack();
+        principal.setVisible(true);
+    }
+}
